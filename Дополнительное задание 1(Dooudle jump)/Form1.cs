ab0dfb233f0eb83f63573eb2a74ef0d8551e9fe5
Dmitry Lvov <voz-demon.ru@mail.ru>
2021-05-09 20:01:53 +0300

    Добавьте файлы проекта.

---
+++ Дополнительное задание 1(Dooudle jump)/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Дополнительное_задание_1_Dooudle_jump_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
